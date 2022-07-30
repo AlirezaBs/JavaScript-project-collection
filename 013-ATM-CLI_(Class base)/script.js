76bf11c418adb0779550bdf7f01c6f8bcb6c6b96
Alireza Babazadeh <aalirezabs@gmail.com>
2022-07-30 18:33:31 +0430

    Update script.js

--- 013-ATM-CLI_(Class base)/script.js
+++ 013-ATM-CLI_(Class base)/script.js
@@ -1,10 +1,10 @@
 // 1. Ask for account ✔️
-// 2. If account does not exist ask to create account
-// 3. Ask what they want to do
-// 4. Execute command
-//   a. View
-//   b. Withdraw
-//   c. Deposit
+// 2. If account does not exist ask to create account ✔️
+// 3. Ask what they want to do ✔️
+// 4. Execute command ✔️
+//   a. View ✔️
+//   b. Withdraw ✔️
+//   c. Deposit ✔️
 
 const Account = require("./Account")
 const CommandLine = require("./CommandLine")
